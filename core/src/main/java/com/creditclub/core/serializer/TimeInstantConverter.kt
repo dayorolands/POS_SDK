b7a8f5338e691f0b6ed7b27d82ab55249e7f8f9d
Emmanuel Nosakhare <enosakhare@appzonegroup.com>
2020-10-03 11:46:48 +0100

    fix(NetworkMetricsInterceptor): Support null values in TimeInstantConverter

--- core/src/main/java/com/creditclub/core/serializer/TimeInstantConverter.kt
+++ core/src/main/java/com/creditclub/core/serializer/TimeInstantConverter.kt
@@ -8,11 +8,11 @@ import java.time.Instant
 
 class TimeInstantConverter : PropertyConverter<Instant, String> {
 
-    override fun convertToDatabaseValue(entityProperty: Instant): String {
-        return entityProperty.format(CREDIT_CLUB_DATE_PATTERN)
+    override fun convertToDatabaseValue(entityProperty: Instant?): String? {
+        return entityProperty?.format(CREDIT_CLUB_DATE_PATTERN)
     }
 
-    override fun convertToEntityProperty(databaseValue: String): Instant {
-        return databaseValue.instantFromPattern(CREDIT_CLUB_DATE_PATTERN)
+    override fun convertToEntityProperty(databaseValue: String?): Instant? {
+        return databaseValue?.instantFromPattern(CREDIT_CLUB_DATE_PATTERN)
     }
 }
