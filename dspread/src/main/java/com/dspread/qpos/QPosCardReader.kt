46e31c493b29841dc04de55ebd71c7c6e3b14141
Emmanuel Nosakhare <enosakhare@appzonegroup.com>
2021-01-23 12:11:36 +0100

    update QPosCardReader.kt

--- dspread/src/main/java/com/dspread/qpos/QPosCardReader.kt
+++ dspread/src/main/java/com/dspread/qpos/QPosCardReader.kt
@@ -1,68 +1,47 @@
 package com.dspread.qpos
 
-import android.Manifest
-import android.app.Activity
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
-import android.content.Intent
 import android.os.Handler
 import android.os.Looper
 import com.creditclub.core.ui.CreditClubActivity
-import com.creditclub.core.util.format
-import com.creditclub.core.util.showErrorAndWait
-import com.creditclub.pos.DukptConfig
 import com.creditclub.pos.PosConfig
-import com.creditclub.pos.PosManager
-import com.creditclub.pos.card.*
-import com.creditclub.pos.utils.TripleDesCipher
-import com.dspread.qpos.utils.FileUtils
-import com.dspread.qpos.utils.hexBytes
+import com.creditclub.pos.PosParameter
+import com.creditclub.pos.card.CardData
+import com.creditclub.pos.card.CardReader
+import com.creditclub.pos.card.CardReaderEvent
+import com.creditclub.pos.card.CardReaderEventListener
+import com.dspread.qpos.utils.*
 import com.dspread.qpos.utils.hexString
 import com.dspread.xpos.QPOSService
-import com.eazypermissions.common.model.PermissionResult
-import com.eazypermissions.coroutinespermission.PermissionManager
 import kotlinx.coroutines.*
 import org.koin.core.KoinComponent
 import org.koin.core.inject
-import java.time.Instant
-import java.util.*
-import kotlin.concurrent.scheduleAtFixedRate
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
-
 
 class QPosCardReader(
     private val activity: CreditClubActivity,
     private val pos: QPOSService,
     private val listener: QPosListener,
-    private val mainScope: CoroutineScope,
-    private val sessionData: PosManager.SessionData
+    private val mainScope: CoroutineScope
 ) : CardReader, KoinComponent {
 
     private val keyIndex = 0
+    private val posParameter: PosParameter by inject()
     private val posConfig: PosConfig by inject()
     private var watchJob: Job? = null
     private val dialogProvider = activity.dialogProvider
 
     override suspend fun waitForCard(): CardReaderEvent {
-        val canUseBluetooth = checkBluetoothPermission()
-        if (!canUseBluetooth) return CardReaderEvent.CANCELLED
-        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-        if (!bluetoothAdapter.isEnabled && !turnOnBluetooth()) {
-            dialogProvider.showErrorAndWait("Could not turn on bluetooth")
-            return CardReaderEvent.CANCELLED
-        }
-
         Looper.myLooper() ?: Looper.prepare()
         val handler = Handler(Looper.myLooper()!!)
         pos.initListener(handler, listener)
         pos.clearBluetoothBuffer()
         pos.setTerminalID(posConfig.terminalId, 5000)
-        val device = findDevice() ?: return CardReaderEvent.CANCELLED
-        val connected: Boolean = connectDevice(device)
-        if (!connected) {
-            dialogProvider.showErrorAndWait("Could not connect to device")
-            return CardReaderEvent.CANCELLED
+        val connected: Boolean = connectDevice()
+        if (!connected) return suspendCoroutine { continuation ->
+            dialogProvider.showError("Could not connect to device") {
+                onClose { continuation.resume(CardReaderEvent.CANCELLED) }
+            }
         }
         dialogProvider.showProgressBar("Securing Connection")
         updateEmvConfig()
@@ -71,46 +50,8 @@ class QPosCardReader(
         return CardReaderEvent.CHIP
     }
 
-    private suspend fun turnOnBluetooth(): Boolean {
-        val turnOn = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-        val result = ActivityResultManager.getActivityResult(activity, turnOn, 2012)
-        return result.resultCode == Activity.RESULT_OK
-    }
-
-    private suspend fun checkBluetoothPermission(): Boolean {
-        val permissionResult = PermissionManager.requestPermissions(
-            activity,
-            2000,
-            Manifest.permission.BLUETOOTH_ADMIN,
-            Manifest.permission.BLUETOOTH,
-        )
-
-        when (permissionResult) {
-            is PermissionResult.PermissionGranted -> {
-                return true
-            }
-            is PermissionResult.PermissionDenied -> {
-                dialogProvider.showErrorAndWait("Bluetooth access is required to use mPOS")
-            }
-            is PermissionResult.PermissionDeniedPermanently -> {
-                dialogProvider.showErrorAndWait(
-                    "Bluetooth access is required to use mPOS.\n" +
-                            "Please manually go to settings and enable permission(s)"
-                )
-            }
-            is PermissionResult.ShowRational -> {
-                dialogProvider.showErrorAndWait("Bluetooth access is required to use mPOS")
-            }
-        }
-
-        return false
-    }
-
     override suspend fun read(amountStr: String): CardData? {
-        val terminalTime = Instant.now().format("HHmmss")
-        val cardNo = listener.waitForString { pos.getIccCardNo(terminalTime) } ?: return null
-        if (cardNo.isBlank()) return QposCardData().apply { status = CardTransactionStatus.Failure }
-        loadKeys(cardNo)
+        loadKeys()
         return suspendCoroutine { continuation ->
             listener.cardDataContinuation = continuation
             pos.doTrade(keyIndex, 60)
@@ -135,30 +76,29 @@ class QPosCardReader(
     private suspend fun updateEmvConfig() {
 //        val emvAppCfg = FileUtils.readAssetsLine("emv_app.bin", activity)?.hexString
 //        val emvCapkCfg = FileUtils.readAssetsLine("emv_capk.bin", activity)?.hexString
-        val emvProfileTlv = String(FileUtils.readAssetsLine("emv_profile_tlv.xml", activity))
+//        val emvProfileTlv = String(FileUtils.readAssetsLine("emv_profile_tlv.xml", activity))
 
 //        listener.waitForUnit { pos.updateEmvConfig(emvAppCfg, emvCapkCfg) }
-        listener.waitForUnit { pos.updateEMVConfigByXml(emvProfileTlv) }
-//        for (emvAid in posParameter.emvAids) {
-//            listener.waitForUnit {
-//                pos.updateEmvAPPByTlv(QPOSService.EMVDataOperation.update, emvAid)
-//            }
-//        }
-//        for (capKey in posParameter.capKeys) {
-//            listener.waitForUnit {
-//                pos.updateEmvCAPKByTlv(QPOSService.EMVDataOperation.update, capKey)
-//            }
-//        }
+//        listener.waitForUnit { pos.updateEMVConfigByXml(emvProfileTlv) }
+        for (emvAid in posParameter.emvAids) {
+            listener.waitForUnit {
+                pos.updateEmvAPPByTlv(QPOSService.EMVDataOperation.update, emvAid)
+            }
+        }
+        for (capKey in posParameter.capKeys) {
+            listener.waitForUnit {
+                pos.updateEmvCAPKByTlv(QPOSService.EMVDataOperation.update, capKey)
+            }
+        }
     }
 
-    private suspend fun loadKeys(cardNo: String) {
-        val amount = sessionData.amount / 100.0
-        val dukptConfig = sessionData.getDukptConfig?.invoke(cardNo, amount)
+    private suspend fun loadKeys() {
+        val bdk = "0123456789ABCDEFFEDCBA9876543210"
         val ksn = "09118012400705E00000"
-        val ipekString = "C22766F7379DD38AA5E1DA8C6AFA75AC"
-        val kcvString = "B2DE27F60A443944" /*Dukpt.encryptTripleDes(ipek, ByteArray(8)).hexString*/
+        val ipek = Dukpt.computeKey(bdk.hexBytes, ksn.hexBytes)
+        val ipekString = ipek.hexString
+        val kcvString = Dukpt.encryptTripleDes(ipek, ByteArray(8)).hexString
 
-        listener.dukptConfig = dukptConfig
         listener.waitForUnit {
             pos.doUpdateIPEKOperation(
                 "01",
@@ -168,52 +108,20 @@ class QPosCardReader(
                 ksn,
                 ipekString,
                 kcvString,
-                dukptConfig?.ksn ?: "09118012400705E00000",
-                dukptConfig?.ipek?.padStart(32) ?: "C22766F7379DD38AA5E1DA8C6AFA75AC",
-                dukptConfig?.kcv ?: "B2DE27F60A443944"
+                "09118012400705E00000",
+                "C22766F7379DD38AA5E1DA8C6AFA75AC",
+                "B2DE27F60A443944"
             )
         }
     }
 
-    private inline val DukptConfig.kcv: String
-        get() = TripleDesCipher(ipek.padStart(32, '0').hexBytes).encrypt(ByteArray(8)).hexString
-//        get() = Dukpt.encryptTripleDes(ipek.hexBytes, ByteArray(8)).hexString
-
-    private suspend fun connectDevice(device: BluetoothDevice): Boolean {
+    private suspend fun connectDevice(): Boolean {
         dialogProvider.showProgressBar("Connecting to mPOS device")
         val connected: Boolean = withContext(Dispatchers.IO) {
-            pos.syncConnectBluetooth(true, 25, device.address)
+//            listener.posConnectContinuation = continuation
+            pos.syncConnectBluetooth(true, 25, "04:D1:6E:54:1E:02")
         }
         dialogProvider.hideProgressBar()
         return connected
-    }
-
-    private suspend fun findDevice(): BluetoothDevice? = suspendCoroutine {
-        pos.scanQPos2Mode(activity, 20)
-        var findDeviceDialog: QPosFindDeviceDialog? = null
-        var timerTask: TimerTask? = null
-        findDeviceDialog = QPosFindDeviceDialog(activity) {
-            onSubmit { device ->
-                timerTask?.cancel()
-                pos.stopScanQPos2Mode()
-                findDeviceDialog?.dismiss()
-                findDeviceDialog = null
-                it.resume(device)
-            }
-
-            onClose {
-                timerTask?.cancel()
-                pos.stopScanQPos2Mode()
-                it.resume(null)
-            }
-        }
-        timerTask = Timer().scheduleAtFixedRate(1000, 1000) {
-            activity.runOnUiThread {
-                findDeviceDialog?.updateList(
-                    pos.deviceList?.filter { it.name.startsWith("MPOS") }
-                )
-            }
-        }
-        findDeviceDialog?.show()
     }
 }
