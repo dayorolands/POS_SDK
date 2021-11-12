c1d3a0fcf74bb561ccb2b63efa4cd18e11d7e4fd
Emmanuel Nosakhare <nonreeactiv@gmail.com>
2021-11-12 10:48:55 +0100

    move pos worker registration to PosApp

--- app/src/main/java/com/appzonegroup/app/fasttrack/utility/extensions/RegisterWorkers.kt
+++ app/src/main/java/com/appzonegroup/app/fasttrack/utility/extensions/RegisterWorkers.kt
@@ -2,10 +2,7 @@ package com.appzonegroup.app.fasttrack.utility.extensions
 
 import android.app.Application
 import androidx.work.*
-import com.appzonegroup.app.fasttrack.work.AppUpdateWorker
 import com.appzonegroup.app.fasttrack.work.MobileTrackingWorker
-import com.appzonegroup.app.fasttrack.work.PosNotificationWorker
-import com.appzonegroup.creditclub.pos.Platform
 import org.koin.ext.getFullName
 import java.util.concurrent.TimeUnit
 
@@ -19,24 +16,11 @@ fun Application.registerWorkers() {
         uniqueWorkName = "APP_MOBILE_TRACKING",
         constraints = constraints,
     )
-
-    if (Platform.isPOS) {
-        workManager.registerPeriodicWorker<PosNotificationWorker>(
-            uniqueWorkName = "APP_POS_NOTIFICATION",
-            constraints = constraints,
-        )
-    }
-    val hasPosUpdateManager = Platform.isPOS && Platform.deviceType != 2
-    if (hasPosUpdateManager) {
-        workManager.registerPeriodicWorker<AppUpdateWorker>(
-            constraints = constraints,
-        )
-    }
 }
 
 inline fun <reified W : ListenableWorker> WorkManager.registerPeriodicWorker(
-    uniqueWorkName: String = W::class.getFullName(),
     constraints: Constraints,
+    uniqueWorkName: String = W::class.getFullName(),
 ) {
     val periodicWorkRequest = PeriodicWorkRequestBuilder<W>(15, TimeUnit.MINUTES)
         .setConstraints(constraints)
