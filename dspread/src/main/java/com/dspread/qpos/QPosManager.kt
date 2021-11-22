511eef39b4d1517b281404f78d234a49cf587f19
Emmanuel Nosakhare <nonreactiv@gmail.com>
2021-11-22 22:04:41 +0100

    inject defaultPosParameter into QPosManager

--- dspread/src/main/java/com/dspread/qpos/QPosManager.kt
+++ dspread/src/main/java/com/dspread/qpos/QPosManager.kt
@@ -7,6 +7,7 @@ import com.creditclub.core.ui.CreditClubActivity
 import com.creditclub.core.ui.widget.DialogProvider
 import com.creditclub.pos.PosManager
 import com.creditclub.pos.PosManagerCompanion
+import com.creditclub.pos.PosParameter
 import com.creditclub.pos.card.CardReader
 import com.creditclub.pos.printer.PosPrinter
 import com.dspread.xpos.QPOSService
@@ -18,15 +19,26 @@ import org.koin.dsl.module
  * Created by Emmanuel Nosakhare <enosakhare@appzonegroup.com> on 04/12/2019.
  * Appzone Ltd
  */
-class QPosManager(activity: CreditClubActivity) : PosManager,
-    DialogProvider by activity.dialogProvider {
-    private val mainScope = CoroutineScope(Dispatchers.Main)
-
+class QPosManager(
+    activity: CreditClubActivity,
+    defaultPosParameter: PosParameter,
+) : PosManager, DialogProvider by activity.dialogProvider {
+    private val mainScope = MainScope()
     internal val pos = QPOSService.getInstance(QPOSService.CommunicationMode.BLUETOOTH)
-    internal val listener = QPosListener(pos, this)
+    private val listener = QPosListener(
+        pos = pos,
+        qPosManager = this,
+        defaultPosParameter = defaultPosParameter,
+    )
 
     override val sessionData = PosManager.SessionData()
-    override val cardReader: CardReader = QPosCardReader(activity, pos, listener, mainScope, sessionData)
+    override val cardReader: CardReader = QPosCardReader(
+        activity = activity,
+        pos = pos,
+        listener = listener,
+        mainScope = mainScope,
+        sessionData = sessionData,
+    )
 
     override suspend fun loadEmv() {
         pos.setContext(activity)
@@ -54,7 +66,7 @@ class QPosManager(activity: CreditClubActivity) : PosManager,
         override val deviceType = 2
         override val module = module {
             factory<PosManager> { (activity: CreditClubActivity) ->
-                QPosManager(activity)
+                QPosManager(activity = activity, defaultPosParameter = get())
             }
             factory<PosPrinter> { (activity: CreditClubActivity) ->
                 QposPrinter(activity)
